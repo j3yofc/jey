8f7b8603d9ea68bd427338151360726c74702c8f
j3yofc <93015795+j3yofc@users.noreply.github.com>
2021-10-22 19:55:05 -0300

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,40 @@
+var caixa;
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  caixa = createSprite(200,200,30,30);
+  caixa.shapeColor ="purple" 
+
+
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    caixa.position.x = caixa.position.x+5;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    caixa.position.x = caixa.position.x-5;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    caixa.position.y = caixa.position.y-5;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    caixa.position.y = caixa.position.y+5;
+  }
+
+  drawSprites();
+
+}
+
+
+
+
